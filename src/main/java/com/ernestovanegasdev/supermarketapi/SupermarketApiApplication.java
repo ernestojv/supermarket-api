b75e75f5d4043eca4528f4469d758be470de5855
Ernesto Vanegas <ernestovanegas9@gmail.com>
2020-09-08 20:22:43 -0600

    Basic Structure

--- src/main/java/com/ernestovanegasdev/supermarketapi/SupermarketApiApplication.java
+++ src/main/java/com/ernestovanegasdev/supermarketapi/SupermarketApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SupermarketApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SupermarketApiApplication.class, args);
 	}
